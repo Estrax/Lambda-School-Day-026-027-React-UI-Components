b1cc78a08d1127110c01459ec32ef79b2ef8e35e
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-11 16:50:20 -0500

    styled CardComponents

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,11 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <img src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
+        <img
+            src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+            className="img-card"
+            alt="Card Banner"
+            />
     );
 }
 
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,8 +3,8 @@ import './Card.css';
 
 const CardContent = () => {
     return (
-        <div>
-            <h1>Get started with React</h1>
+        <div className="card-content">
+            <h2>Get started with React!</h2>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
             <a href="https://reactjs.org">reactjs.org</a>
         </div>
