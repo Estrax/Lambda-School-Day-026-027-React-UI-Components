df7a9eb8924b360d82971a6853ac20f236b25978
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-11 15:47:53 -0500

    added HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    const currDate = new Date();
+    
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <h2>@LambdaSchool</h2>
+            <p>{currDate.getDate()} {currDate.toLocaleString('en-us', { month: 'short' }).toLowerCase()}</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
