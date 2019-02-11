527e231afe03385207ae75c1046a7861dab14336
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-11 18:50:58 -0500

    refactored operators

--- Project-II-Calculator/calculator/src/components/CalculatorComponents/OperatorsPanel.js
+++ Project-II-Calculator/calculator/src/components/CalculatorComponents/OperatorsPanel.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ActionButton from '../ButtonComponents/ActionButton';
 
+const operators = [
+    {
+        id: 'operator-1',
+        text: '/',
+        style: 'action operator long'
+    },
+    {
+        id: 'operator-2',
+        text: 'x',
+        style: 'action operator long'
+    },
+    {
+        id: 'operator-3',
+        text: '-',
+        style: 'action operator long'
+    },
+    {
+        id: 'operator-4',
+        text: '+',
+        style: 'action operator long'
+    },
+    {
+        id: 'operator-5',
+        text: '=',
+        style: 'action operator long'
+    }
+];
 
 const OperatorsPanel = () => {
     return (
         <div className="operators-panel calc-inline">
-            <ActionButton text="/" buttonStyle="action operator long"/>
-            <ActionButton text="x" buttonStyle="action operator long"/>
-            <ActionButton text="-" buttonStyle="action operator long"/>
-            <ActionButton text="+" buttonStyle="action operator long"/>
-            <ActionButton text="=" buttonStyle="action operator long"/>
+            {operators.map(operator => 
+                <ActionButton key={operator.id} id={operator.id} text={operator.text} buttonStyle={operator.style}/>
+            )}
         </div>
     );
 };
